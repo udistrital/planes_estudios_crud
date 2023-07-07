ac1c97bce04b0a14021d34452e8e835e7c610a17
fabianbarreto02 <fabibeto11@hotmail.com>
2023-07-07 18:43:58 -0500

    managment: Ajuste de migracitions por cambio de nombre de esquema

--- database/scripts/20230625_184132_creacion_modelo_planes_estudios_up.sql
+++ database/scripts/20230625_184132_creacion_modelo_planes_estudios_up.sql
@@ -13,19 +13,19 @@ CREATE DATABASE new_database;
 -- ddl-end --
 
 
--- object: planes_estudios_crud | type: SCHEMA --
--- DROP SCHEMA IF EXISTS planes_estudios_crud CASCADE;
-CREATE SCHEMA planes_estudios_crud;
+-- object: plan_estudios | type: SCHEMA --
+-- DROP SCHEMA IF EXISTS plan_estudios CASCADE;
+CREATE SCHEMA plan_estudios;
 -- ddl-end --
-ALTER SCHEMA planes_estudios_crud OWNER TO postgres;
--- ddl-end --
-
-SET search_path TO pg_catalog,public,planes_estudios_crud;
+ALTER SCHEMA plan_estudios OWNER TO postgres;
 -- ddl-end --
 
--- object: planes_estudios_crud.plan_estudio | type: TABLE --
--- DROP TABLE IF EXISTS planes_estudios_crud.plan_estudio CASCADE;
-CREATE TABLE planes_estudios_crud.plan_estudio (
+SET search_path TO pg_catalog,public,plan_estudios;
+-- ddl-end --
+
+-- object: plan_estudios.plan_estudio | type: TABLE --
+-- DROP TABLE IF EXISTS plan_estudios.plan_estudio CASCADE;
+CREATE TABLE plan_estudios.plan_estudio (
 	id serial NOT NULL,
 	nombre character varying(50) NOT NULL,
 	codigo_abreaviacion character varying(50),
@@ -47,44 +47,44 @@ CREATE TABLE planes_estudios_crud.plan_estudio (
 	CONSTRAINT plan_estudio_pk PRIMARY KEY (id)
 );
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.codigo IS E'Campo para el manejo del codigo del plan de estudios';
+COMMENT ON COLUMN plan_estudios.plan_estudio.codigo IS E'Campo para el manejo del codigo del plan de estudios';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.codigo_abreaviacion IS E'Campo para el manejo del codigo de abreaviacion para el plan de estudios';
+COMMENT ON COLUMN plan_estudios.plan_estudio.codigo_abreaviacion IS E'Campo para el manejo del codigo de abreaviacion para el plan de estudios';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.proyecto_academico_id IS E'campo para la relacion del proyecto academico del plan de estudios';
+COMMENT ON COLUMN plan_estudios.plan_estudio.proyecto_academico_id IS E'campo para la relacion del proyecto academico del plan de estudios';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.total_creditos IS E'Campo para el registro del total de creditos';
+COMMENT ON COLUMN plan_estudios.plan_estudio.total_creditos IS E'Campo para el registro del total de creditos';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.numero_semestres IS E'Campo para almacenar el numero de semestres del plan de estudios';
+COMMENT ON COLUMN plan_estudios.plan_estudio.numero_semestres IS E'Campo para almacenar el numero de semestres del plan de estudios';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.numero_resolucion IS E'Campo para almacenar el numero de la resolucion del plan de estudios';
+COMMENT ON COLUMN plan_estudios.plan_estudio.numero_resolucion IS E'Campo para almacenar el numero de la resolucion del plan de estudios';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.ano_resolucion IS E'Campo para el manejo del ano de la resolucion del plan de estuidos';
+COMMENT ON COLUMN plan_estudios.plan_estudio.ano_resolucion IS E'Campo para el manejo del ano de la resolucion del plan de estuidos';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.espacios_semestre_distribucion IS E'Campo para almancenar la distribucion de los espacios academicos para el plan de estudios';
+COMMENT ON COLUMN plan_estudios.plan_estudio.espacios_semestre_distribucion IS E'Campo para almancenar la distribucion de los espacios academicos para el plan de estudios';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.resumen_plan_estudios IS E'Campo para almancenar el resumen del plan de estudios';
+COMMENT ON COLUMN plan_estudios.plan_estudio.resumen_plan_estudios IS E'Campo para almancenar el resumen del plan de estudios';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.soporte_documental IS E'Campo para la relacion de los documentos del plan de estudios';
+COMMENT ON COLUMN plan_estudios.plan_estudio.soporte_documental IS E'Campo para la relacion de los documentos del plan de estudios';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.observacion IS E'Campo para el registro de las observaciones de los planes de estudios';
+COMMENT ON COLUMN plan_estudios.plan_estudio.observacion IS E'Campo para el registro de las observaciones de los planes de estudios';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.activo IS E'campo para el estado del registro';
+COMMENT ON COLUMN plan_estudios.plan_estudio.activo IS E'campo para el estado del registro';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.fecha_creacion IS E'Campo para el registro de la fecha de creacion del registro';
+COMMENT ON COLUMN plan_estudios.plan_estudio.fecha_creacion IS E'Campo para el registro de la fecha de creacion del registro';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.fecha_modificacion IS E'Campo para el registro de la fecha de modificacion del registro';
+COMMENT ON COLUMN plan_estudios.plan_estudio.fecha_modificacion IS E'Campo para el registro de la fecha de modificacion del registro';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.plan_estudio_padre_id IS E'Campo para la relacione entre planes de estudios';
+COMMENT ON COLUMN plan_estudios.plan_estudio.plan_estudio_padre_id IS E'Campo para la relacione entre planes de estudios';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio.estado_aprobacion_id IS E'Campo para el registro del estado del plan de estudio';
+COMMENT ON COLUMN plan_estudios.plan_estudio.estado_aprobacion_id IS E'Campo para el registro del estado del plan de estudio';
 -- ddl-end --
-ALTER TABLE planes_estudios_crud.plan_estudio OWNER TO postgres;
+ALTER TABLE plan_estudios.plan_estudio OWNER TO postgres;
 -- ddl-end --
 
--- object: planes_estudios_crud.estado_aprobacion | type: TABLE --
--- DROP TABLE IF EXISTS planes_estudios_crud.estado_aprobacion CASCADE;
-CREATE TABLE planes_estudios_crud.estado_aprobacion (
+-- object: plan_estudios.estado_aprobacion | type: TABLE --
+-- DROP TABLE IF EXISTS plan_estudios.estado_aprobacion CASCADE;
+CREATE TABLE plan_estudios.estado_aprobacion (
 	id serial NOT NULL,
 	nombre character varying(50) NOT NULL,
 	descripcion character varying(250),
@@ -95,26 +95,26 @@ CREATE TABLE planes_estudios_crud.estado_aprobacion (
 	CONSTRAINT estado_aprobacion_pk PRIMARY KEY (id)
 );
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.estado_aprobacion.id IS E'Campo serial';
+COMMENT ON COLUMN plan_estudios.estado_aprobacion.id IS E'Campo serial';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.estado_aprobacion.nombre IS E'Campo para el registro del nombre del estado';
+COMMENT ON COLUMN plan_estudios.estado_aprobacion.nombre IS E'Campo para el registro del nombre del estado';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.estado_aprobacion.descripcion IS E'Campo para la descrición del registro';
+COMMENT ON COLUMN plan_estudios.estado_aprobacion.descripcion IS E'Campo para la descrición del registro';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.estado_aprobacion.codigo_abreviacion IS E'Campo para el manejo del codigo de abreviacion';
+COMMENT ON COLUMN plan_estudios.estado_aprobacion.codigo_abreviacion IS E'Campo para el manejo del codigo de abreviacion';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.estado_aprobacion.activo IS E'Campo para el estado del registro';
+COMMENT ON COLUMN plan_estudios.estado_aprobacion.activo IS E'Campo para el estado del registro';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.estado_aprobacion.fecha_creacion IS E'Campo para el registo de la fecha de creacion';
+COMMENT ON COLUMN plan_estudios.estado_aprobacion.fecha_creacion IS E'Campo para el registo de la fecha de creacion';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.estado_aprobacion.fecha_modificacion IS E'Campo para el registro de la fecha de modificacion';
+COMMENT ON COLUMN plan_estudios.estado_aprobacion.fecha_modificacion IS E'Campo para el registro de la fecha de modificacion';
 -- ddl-end --
-ALTER TABLE planes_estudios_crud.estado_aprobacion OWNER TO postgres;
+ALTER TABLE plan_estudios.estado_aprobacion OWNER TO postgres;
 -- ddl-end --
 
--- object: planes_estudios_crud.plan_estudio_proyecto_academico | type: TABLE --
--- DROP TABLE IF EXISTS planes_estudios_crud.plan_estudio_proyecto_academico CASCADE;
-CREATE TABLE planes_estudios_crud.plan_estudio_proyecto_academico (
+-- object: plan_estudios.plan_estudio_proyecto_academico | type: TABLE --
+-- DROP TABLE IF EXISTS plan_estudios.plan_estudio_proyecto_academico CASCADE;
+CREATE TABLE plan_estudios.plan_estudio_proyecto_academico (
 	id serial NOT NULL,
 	plan_estudio_id integer NOT NULL,
 	proyecto_academico_id integer NOT NULL,
@@ -125,39 +125,39 @@ CREATE TABLE planes_estudios_crud.plan_estudio_proyecto_academico (
 	CONSTRAINT plan_estudio_proyecto_academico_pk PRIMARY KEY (id)
 );
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio_proyecto_academico.plan_estudio_id IS E'Campo para la referecia del plan de estudio';
+COMMENT ON COLUMN plan_estudios.plan_estudio_proyecto_academico.plan_estudio_id IS E'Campo para la referecia del plan de estudio';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio_proyecto_academico.proyecto_academico_id IS E'campo para el registro del proyecto academico';
+COMMENT ON COLUMN plan_estudios.plan_estudio_proyecto_academico.proyecto_academico_id IS E'campo para el registro del proyecto academico';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio_proyecto_academico.orden_proyecto IS E'Campos para el registro del orden los proyectos en un plan de estudio compartido';
+COMMENT ON COLUMN plan_estudios.plan_estudio_proyecto_academico.orden_proyecto IS E'Campos para el registro del orden los proyectos en un plan de estudio compartido';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio_proyecto_academico.activo IS E'Campo para el registro del estado';
+COMMENT ON COLUMN plan_estudios.plan_estudio_proyecto_academico.activo IS E'Campo para el registro del estado';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio_proyecto_academico.fecha_creacion IS E'Campo para el registro de la fecha de creacion';
+COMMENT ON COLUMN plan_estudios.plan_estudio_proyecto_academico.fecha_creacion IS E'Campo para el registro de la fecha de creacion';
 -- ddl-end --
-COMMENT ON COLUMN planes_estudios_crud.plan_estudio_proyecto_academico.fecha_modificacion IS E'Campo para el registro de la fecha de modificacion';
+COMMENT ON COLUMN plan_estudios.plan_estudio_proyecto_academico.fecha_modificacion IS E'Campo para el registro de la fecha de modificacion';
 -- ddl-end --
-ALTER TABLE planes_estudios_crud.plan_estudio_proyecto_academico OWNER TO postgres;
+ALTER TABLE plan_estudios.plan_estudio_proyecto_academico OWNER TO postgres;
 -- ddl-end --
 
 -- object: rel_estado_aprobacion_plan_estudio | type: CONSTRAINT --
--- ALTER TABLE planes_estudios_crud.plan_estudio DROP CONSTRAINT IF EXISTS rel_estado_aprobacion_plan_estudio CASCADE;
-ALTER TABLE planes_estudios_crud.plan_estudio ADD CONSTRAINT rel_estado_aprobacion_plan_estudio FOREIGN KEY (estado_aprobacion_id)
-REFERENCES planes_estudios_crud.estado_aprobacion (id) MATCH SIMPLE
+-- ALTER TABLE plan_estudios.plan_estudio DROP CONSTRAINT IF EXISTS rel_estado_aprobacion_plan_estudio CASCADE;
+ALTER TABLE plan_estudios.plan_estudio ADD CONSTRAINT rel_estado_aprobacion_plan_estudio FOREIGN KEY (estado_aprobacion_id)
+REFERENCES plan_estudios.estado_aprobacion (id) MATCH SIMPLE
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 -- ddl-end --
 
 -- object: rel_plan_estudio_plan_estudio | type: CONSTRAINT --
--- ALTER TABLE planes_estudios_crud.plan_estudio DROP CONSTRAINT IF EXISTS rel_plan_estudio_plan_estudio CASCADE;
-ALTER TABLE planes_estudios_crud.plan_estudio ADD CONSTRAINT rel_plan_estudio_plan_estudio FOREIGN KEY (plan_estudio_padre_id)
-REFERENCES planes_estudios_crud.plan_estudio (id) MATCH SIMPLE
+-- ALTER TABLE plan_estudios.plan_estudio DROP CONSTRAINT IF EXISTS rel_plan_estudio_plan_estudio CASCADE;
+ALTER TABLE plan_estudios.plan_estudio ADD CONSTRAINT rel_plan_estudio_plan_estudio FOREIGN KEY (plan_estudio_padre_id)
+REFERENCES plan_estudios.plan_estudio (id) MATCH SIMPLE
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 -- ddl-end --
 
 -- object: rel_plan_estudio_plan_estudio_proyecto_academico | type: CONSTRAINT --
--- ALTER TABLE planes_estudios_crud.plan_estudio_proyecto_academico DROP CONSTRAINT IF EXISTS rel_plan_estudio_plan_estudio_proyecto_academico CASCADE;
-ALTER TABLE planes_estudios_crud.plan_estudio_proyecto_academico ADD CONSTRAINT rel_plan_estudio_plan_estudio_proyecto_academico FOREIGN KEY (plan_estudio_id)
-REFERENCES planes_estudios_crud.plan_estudio (id) MATCH SIMPLE
+-- ALTER TABLE plan_estudios.plan_estudio_proyecto_academico DROP CONSTRAINT IF EXISTS rel_plan_estudio_plan_estudio_proyecto_academico CASCADE;
+ALTER TABLE plan_estudios.plan_estudio_proyecto_academico ADD CONSTRAINT rel_plan_estudio_plan_estudio_proyecto_academico FOREIGN KEY (plan_estudio_id)
+REFERENCES plan_estudios.plan_estudio (id) MATCH SIMPLE
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 -- ddl-end --
 
